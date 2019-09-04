1c4e8e1d64428dfe948d8f1eeeff2f4402b9e843
Viktor Mitin <viktor.mitin.19@gmail.com>
2019-09-04 10:22:23 +0300

    vpn_touch.sh: fixed path to vpn.exp

--- vpn_touch.sh
+++ vpn_touch.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+date > ~/w/tr_vpn.log
+
 if /opt/cisco/anyconnect/bin/vpn state | grep -q Connected ; then
     echo Connected to VPN, disconnecting...
     /opt/cisco/anyconnect/bin/vpn disconnect
     sudo sh -c "setleds -L -num < /dev/tty7"
 else
     echo NOT Connected to VPN, connecting...
-    ~/configs/vpn.exp
+    ~/w/TR/vpn.exp
     sudo sh -c "setleds -L +num < /dev/tty7"
 fi
 
