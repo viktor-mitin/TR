a7324185ccb9c4c122221bef4fd413b27a40a07e
Viktor Mitin <viktor.mitin.19@gmail.com>
2019-09-04 14:45:03 +0300

    run_glue_job.sh: added several improvements

--- run_glue_job.sh
+++ run_glue_job.sh
@@ -2,35 +2,60 @@
 
 #############################################################################
 #
-# This script runs given glue job and prints it's output to stdout
-# The first parameter is job name to run
+# This script runs given AWS glue job and prints it's output to stdout.
+# The first parameter is job name to run.
 #
 # Example: ./run_glue_job.sh Tests
 #
 #############################################################################
 
-set -x
+#set -x
 set -e
+set -E
 set -u
 
 JOB_NAME="$1"
+SLEEP_TIME=3
 
-#get job run id
-jr=$(aws glue start-job-run --job-name $JOB_NAME  | jq '.JobRunId' | tr -d '"')
-
-#get job run status, wait job to complete, TBD
-while true; do
-    STATE=$(aws glue get-job-run --job-name $JOB_NAME --run-id $jr)
-    echo $STATE #debug only
-
-    echo $STATE | grep -v FAILED || break
-    sleep 3
+#check if there are running jobs and wait until completed
+while aws glue get-job-runs --job-name Tests | grep -q RUNNING; do
+    echo "The job is already running, wait and retry"
+    sleep $SLEEP_TIME
 done
 
+#get job run id
+JR=$(aws glue start-job-run --job-name $JOB_NAME  | jq '.JobRunId' | tr -d '"')
+echo "Job Run Id: $JR"
+sleep $SLEEP_TIME
+
+#get job run status, wait job to complete
+while true; do
+    STATE=$(aws glue get-job-run --job-name $JOB_NAME --run-id $JR)
+    echo -n $STATE | jq '.JobRun.JobRunState' #debug only
+    echo $STATE | grep -q CompletedOn && break
+    sleep $SLEEP_TIME
+done
+
+if echo $STATE | grep -q FAILED; then
+    echo
+    echo "ERROR - the job has FAILED with the next errors:"
+    echo
+    #get job run output
+    OUTPUT=$(aws logs get-log-events --log-group-name /aws-glue/python-jobs/error\
+                  --log-stream-name $JR | jq '.events[0] | .message' | tr -d '"' )
+
+    #pretty print output
+    echo "$OUTPUT"
+    echo
+fi
+
 #get job run output
+echo
+#echo "The job run output:"
 OUTPUT=$(aws logs get-log-events --log-group-name /aws-glue/python-jobs/output\
-              --log-stream-name $jr | jq '.events[0] | .message' | tr -d '"' )
+              --log-stream-name $JR | jq '.events[0] | .message' | tr -d '"' )
 
 #pretty print output
-echo -e $OUTPUT
+echo "$OUTPUT"
+echo
 
