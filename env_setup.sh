a17c6191ef3219c1d7dd5aa50e3f133223d658d1
Viktor Mitin <viktor.mitin.19@gmail.com>
2019-09-12 09:43:07 +0300

    env_setup.sh: added new script which prepares TR environment completely

---
+++ env_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+date > ~/w/tr_env_setup.log
+
+if /opt/cisco/anyconnect/bin/vpn state | grep -q Connected ; then
+    echo Connected to VPN, ok
+else
+    echo NOT Connected to VPN, connecting...
+    ~/w/TR/vpn.exp
+    sudo sh -c "setleds -L +num < /dev/tty7"
+fi
+
+
+echo Vault login to cloud-tool
+cloud-tool -vv vault-login  -m `vipaccess` --account-id '074929092668' --role 'human-role/a205718-PowerUser2'
+
+echo Get vault pass
+~/w/TR/get_vault_pass.py
+
+
+echo AWS login and Chrome restart
+DISPLAY=:0.0 ~/w/TR/chrom_restart.sh
+
