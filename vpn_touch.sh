df01aef8336672672484f0f7c513df3108ccffde
Viktor Mitin <viktor.mitin.19@gmail.com>
2019-10-29 16:45:05 +0200

    vpn_touch.sh: added workaround for the vpn connection issue

--- vpn_touch.sh
+++ vpn_touch.sh
@@ -8,6 +8,12 @@ if /opt/cisco/anyconnect/bin/vpn state | grep -q Connected ; then
     sudo sh -c "setleds -L -num < /dev/tty7"
 else
     echo NOT Connected to VPN, connecting...
+
+    # There is a strange bug, that sometimes VPN connection
+    # cannot be established without vpnagentd service restart
+    sudo service vpnagentd restart
+    sleep 2
+
     ~/w/TR/vpn.exp
     sudo sh -c "setleds -L +num < /dev/tty7"
 fi
