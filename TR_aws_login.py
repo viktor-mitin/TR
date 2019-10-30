49c880240db8a85c6f8647f287c786f6bf49854c
Viktor Mitin <viktor.mitin.19@gmail.com>
2019-10-30 10:11:56 +0200

    TR_aws_login.py: added next step - chosing profile

--- TR_aws_login.py
+++ TR_aws_login.py
@@ -33,7 +33,7 @@ options.add_experimental_option("excludeSwitches", ['enable-automation']);
 
 driver = webdriver.Chrome(chrome_options=options)
 
-driver.implicitly_wait(9)
+driver.implicitly_wait(7)
 #driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL + 't')
 driver.get(url)
 
@@ -42,7 +42,7 @@ try:
     elem.click()
 #except NoSuchElementException as e:
 except Exception as e:
-    pass #ignore for now since this button may not appear sometimes
+    pass #ignore, since this button may not appear sometimes
 #    print("------ exception happened: SignInButton")
 #    print(str(e))
 
@@ -55,27 +55,35 @@ except Exception as e:
     print(str(e))
 
 #time.sleep(2)
-#try:
-elem = driver.find_element_by_xpath("//*[contains(@id,'userNameInput')]")
-elem.send_keys("mc261016@mgmt.tlrg.com")
+try:
+    elem = driver.find_element_by_xpath("//*[contains(@id,'userNameInput')]")
+    elem.send_keys("mc261016@mgmt.tlrg.com")
 
-elem = driver.find_element_by_xpath("//*[contains(@id,'passwordInput')]")
+    elem = driver.find_element_by_xpath("//*[contains(@id,'passwordInput')]")
 
-with open(file_path, 'r') as file:
-    p = file.read().replace('\n', '')
+    with open(file_path, 'r') as file:
+        p = file.read().replace('\n', '')
 
-elem.send_keys(p)
-elem.send_keys(Keys.RETURN)
+    elem.send_keys(p)
+    elem.send_keys(Keys.RETURN)
 
-#print("entered credentials")
-#elem = driver.find_element_by_xpath("//input[contains(@id,'idp_GoButton')]")
-#elem.click()
-#print("clicked go button")
-##except:
-##    None
+except Exception as e:
+    pass #ignore, since this the user session may already has been established
+
+print("after entered credentials")
+
+elem = driver.find_element_by_xpath("//input[contains(@id,'arn:aws:iam::074929092668:role/human-role/a205718-PowerUser2')]")
+elem.click()
+print("after radio button")
+
+elem = driver.find_element_by_xpath("//a[contains(@id,'signin_button')]")
+elem.click()
+print("after profile choise")
 
 #time.sleep(3)
 #driver.close()
 #print("driver closed")
+
+print("done")
 sys.exit(0)
 
