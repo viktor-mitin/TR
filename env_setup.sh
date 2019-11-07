56d29a6a19d9403ae31cfc7ff0c3dc9d3561c565
Viktor Mitin <viktor.mitin.19@gmail.com>
2019-11-07 11:03:20 +0200

    env_setup.sh: added minor refactoring and comments about known issues

--- env_setup.sh
+++ env_setup.sh
@@ -1,5 +1,30 @@
 #!/bin/sh -x
 
+#################################################################################
+# This script configures TR aws environment and opens chrome with AWS console
+# It uses vipaccess MFA tool, expect, cisco anyconnect, cloud-tool,
+# selenium and chrome.
+#
+# Known Symantec VIP MFA issues and solutions:
+# - The issue: vipaccess returns correct codes, however MFA does not work.
+# Solution - go to https://vip.symantec.com/ and retest credentials ID.
+# It will synchronize MFA state between symantec servers and your devices.
+# After it MFA should work properly again.
+#
+# - The issue: after TR password change, cloud-tool may ask to enter
+# the new password again. It asks it only once, and then it will not ask it again.
+#
+#################################################################################
+
+### This function logins to AWS CLI with cloud-tool.
+### The first parameter is profile name option - optional.
+cloud_tool_login ()
+{
+    ~/.local/bin/cloud-tool -vvv vault-login $1 \
+              -m `/home/c/.local/bin/vipaccess` \
+              --account-id '074929092668' --role 'human-role/a205718-PowerUser2'
+}
+
 date
 
 if /opt/cisco/anyconnect/bin/vpn state | grep -q Connected ; then
@@ -7,18 +32,22 @@ if /opt/cisco/anyconnect/bin/vpn state | grep -q Connected ; then
 else
     echo NOT Connected to VPN, connecting...
     ~/w/TR/vpn.exp
+
+    #The next command enables NumLock Led. This is cosmetic feature.
+    #Known issue: setleds may not work properly (not stable) in case when it is
+    #started from xserver (GUI).
     sudo sh -c "setleds -L +num < /dev/tty7"
 fi
 
 echo Vault login to cloud-tool
 
+
 ### default profile
-~/.local/bin/cloud-tool -vv vault-login  -m `/home/c/.local/bin/vipaccess` \
-          --account-id '074929092668' --role 'human-role/a205718-PowerUser2'
+cloud_tool_login
 
 ### tr-authorities-preprod profile
-~/.local/bin/cloud-tool -vv --profile tr-authorities-preprod vault-login  -m `/home/c/.local/bin/vipaccess` \
-          --account-id '074929092668' --role 'human-role/a205718-PowerUser2'
+cloud_tool_login "--profile tr-authorities-preprod vault-login"
+
 
 echo Get vault pass
 ~/w/TR/get_vault_pass.py
